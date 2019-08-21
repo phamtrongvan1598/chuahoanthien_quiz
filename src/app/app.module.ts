5ca5c1903e768eb0127468127b9d6e91f331c486
anhnt2989 <anhnt2989@gmail.com>
2019-08-21 22:13:58 +0700

    initialized component, model, service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/main-header/header.component';
+import { FooterComponent } from './component/main-footer/footer.component';
+import { MainBodyComponent } from './component/main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
