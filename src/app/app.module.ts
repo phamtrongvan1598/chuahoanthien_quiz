2193520d7e505b839f98c8645ac0d68fdea40a5f
anhnt2989 <anhnt2989@gmail.com>
2019-08-22 00:18:12 +0700

    add modal for login and sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/main-header/header.component';
 import { FooterComponent } from './component/main-footer/footer.component';
 import { MainBodyComponent } from './component/main-body/main-body.component';
+import { DetailSidenavComponent } from './component/detail/detail-sidenav/detail-sidenav.component';
+import { DetailItemComponent } from './component/detail/detail-item/detail-item.component';
+import { DetailContentComponent } from './component/detail/detail-content/detail-content.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainBodyComponent
+    MainBodyComponent,
+    DetailSidenavComponent,
+    DetailItemComponent,
+    DetailContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
