9ef9850aff82b44a497cf52977ff3fcad1dda06c
anhnt2989 <anhnt2989@gmail.com>
2019-08-24 20:01:24 +0700

    done formed user service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -12,12 +12,24 @@ const httpOption = {
   providedIn: 'root'
 })
 export class UserService {
-  private userUrl = '/api/owner/';
+  private userUrl = '/api/auth/';
 
   constructor(private http: HttpClient) {
   }
 
   getUserBoard(): Observable<string> {
     return this.http.get(this.userUrl, {responseType: 'text'});
+  }
+
+  register(data: FormData): Observable<any> {
+    return this.http.post(this.userUrl + '/signup', data);
+  }
+
+  login(data: FormData): Observable<any> {
+    return this.http.post(this.userUrl + 'signin', data);
+  }
+
+  logout(id: number): Observable<any> {
+    return this.http.post(this.userUrl + 'signout', id);
   }
 }
