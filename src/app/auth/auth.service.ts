a44696da62dfae734f832c02fd3b19246e76e450
anhnt2989 <anhnt2989@gmail.com>
2019-08-27 00:21:37 +0700

    done authService and login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {JwtResponse} from './jwt-response';
 import {SigninInfoService} from './signin-info.service';
+import {FormGroup} from '@angular/forms';
 
 const httpOption = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -12,17 +13,20 @@ const httpOption = {
   providedIn: 'root'
 })
 export class AuthService {
-  private signinUrl = '/api/auth/signin';
-  private signupUrl = '/api/auth/signup';
+  private readonly API_URL = 'http://localhost:8080/api/signin';
+  token: string;
+  header: HttpHeaders;
 
-  constructor(private http: HttpClient) {
+  login(loginForm: FormGroup): Observable<any> {
+    return this.httpClient.post(this.API_URL, JSON.stringify(loginForm), httpOption);
   }
 
-  attemptAuth(credentials: SigninInfoService): Observable<JwtResponse> {
-    return this.http.post<JwtResponse>(this.signinUrl, credentials, httpOption);
-  }
-
-  signUp(info: FormData): Observable<any> {
-    return this.http.post(this.signupUrl, info);
+  constructor(private httpClient: HttpClient) {
+    this.token = localStorage.getItem('token');
+    this.header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`
+    })
+    ;
   }
 }
--- src/app/component/signin/signin.component.ts
+++ src/app/component/signin/signin.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
+import {Router} from '@angular/router';
+import {HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  }
 
   ngOnInit() {
+    this.signinForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    console.log('Log in');
+    console.log(JSON.stringify(this.signinForm.value));
+    this.authService.login(this.signinForm.value).subscribe(
+      next => {
+        localStorage.setItem('token', next.token);
+        this.authService.token = next.token;
+        this.authService.header = new HttpHeaders(
+          {
+            Authorization: `Bearer ${this.authService.token}`,
+            'Content-Type': 'application/json'
+          }
+        );
+        if (next.token) {
+          this.router.navigateByUrl('');
+        }
+        console.log(next.token);
+      }
+    );
   }
 
 }
