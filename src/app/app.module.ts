835dc6bc8be6be25077f1132c98421c9a57e944f
vqn <vuquangnam1507@gmail.com>
2019-08-26 10:53:58 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './component/main-footer/footer.component';
 import { MainBodyComponent } from './component/main-body/main-body.component';
 import { DetailSidenavComponent } from './component/detail/detail-sidenav/detail-sidenav.component';
 import { DetailItemComponent } from './component/detail/detail-item/detail-item.component';
-import { DetailContentComponent } from './component/detail/detail-content/detail-content.component';
+// import { DetailContentComponent } from './component/detail/detail-content/detail-content.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {DetailGroupviewComponent} from './component/detail/detail-groupview/detail-groupview.component';
 
@@ -20,7 +20,7 @@ import {DetailGroupviewComponent} from './component/detail/detail-groupview/deta
     MainBodyComponent,
     DetailSidenavComponent,
     DetailItemComponent,
-    DetailContentComponent,
+    // DetailContentComponent,
     DetailGroupviewComponent
   ],
   imports: [
