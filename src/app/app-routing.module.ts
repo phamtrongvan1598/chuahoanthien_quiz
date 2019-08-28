a93b25e580a24453d2d116c3f383d52f382be512
anhnt2989 <anhnt2989@gmail.com>
2019-08-29 00:55:43 +0700

    dividing banner done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {SignupComponent} from './component/signup/signup.component';
 import {SigninComponent} from './component/signin/signin.component';
 import {MainBodyComponent} from './component/main-body/main-body.component';
+import {MainBannerComponent} from './component/main-banner/main-banner.component';
 
 
 const routes: Routes = [
