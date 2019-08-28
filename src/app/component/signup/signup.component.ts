d1c3e4ae1811bc37093cde35a40c9062f0d54a6d
anhnt2989 <anhnt2989@gmail.com>
2019-08-29 02:17:34 +0700

    edit inputs error messages

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../service/user.service';
 import {Router} from '@angular/router';
+import {log} from 'util';
 
 @Component({
   selector: 'app-signup',
@@ -10,21 +11,21 @@ import {Router} from '@angular/router';
 })
 export class SignupComponent implements OnInit {
   registerForm: FormGroup;
-  isSignedUp = false;
-  isSignUpFailed = false;
+  isAgreedWithLicenses = false;
 
-  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) { }
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
+  }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      username: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      address: [''],
-      dob: [''],
-      phoneNumber: ['', Validators.required, Validators.pattern(/0([0-9]{9,10})/)],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required]
+      email: [null, [Validators.required, Validators.email]],
+      username: [null, [Validators.required, Validators.minLength(4)]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      address: [null],
+      dob: [null],
+      phoneNumber: [null, Validators.required, Validators.pattern(/0([0-9]{9,10})/)],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required]
     });
   }
 
@@ -33,6 +34,7 @@ export class SignupComponent implements OnInit {
       return;
     }
     console.log(this.registerForm.value);
+    console.log(this.isAgreedWithLicenses);
     this.userService.register(this.registerForm.value)
       .subscribe(
         data => {
@@ -44,6 +46,29 @@ export class SignupComponent implements OnInit {
           this.router.navigate(['/api/auth/signup']);
         }
       );
+  }
+
+  get firstName() {
+    return this.registerForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.registerForm.get('lastName');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get phoneNumber() {
+    return this.registerForm.get('phoneNumber');
   }
 
 }
