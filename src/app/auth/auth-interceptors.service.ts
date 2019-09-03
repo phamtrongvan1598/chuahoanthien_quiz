20bde3277909661edabadab7ae869f42e829051f
anhnt2989 <anhnt2989@gmail.com>
2019-09-03 16:20:20 +0700

    remove unnecessary imports

--- src/app/auth/auth-interceptors.service.ts
+++ src/app/auth/auth-interceptors.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {TokenStorageService} from './token-storage.service';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 const TOKEN_HEADER_KEY = 'Authorization';
